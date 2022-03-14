0e35643ad7194256caf77e2cf1f7284dfd809f8e
Anthony Torredes (Tony) <atorrede@emeal.nttdata.com>
2022-03-13 21:12:06 -0500

    mejoras

--- projects/mf-name/src/app/app-routing.module.ts
+++ projects/mf-name/src/app/app-routing.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { loadRemoteModule } from '@angular-architects/module-federation';
 
 const routes: Routes = [
   {
     path: 'mf',
-    loadChildren: () => loadRemoteModule({
-      type: 'module',
-      remoteEntry: 'http://localhost:4201/remoteEntry.js',
-      exposedModule: './showinfo'
-    }).then((m) => m.ShowinfoModule),
+    loadChildren: () =>
+      import('./showinfo/showinfo.module').then((m) => m.ShowinfoModule),
   },
 ];
 
--- projects/mf-name/src/main.ts
+++ projects/mf-name/src/main.ts
@@ -1,4 +1,2 @@
-declare module 'mf/showinfo';
-
 import('./bootstrap')
 	.catch(err => console.error(err));
